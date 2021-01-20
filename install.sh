023deb0b9771d3f674bb829f292095f54ba705ea
ParadoxPod <paradoxpod@gmail.com>
2021-01-20 12:20:16 -0800

    Added some debugging features

--- install.sh
+++ install.sh
@@ -1,13 +1,19 @@
+[ $1 == "-c" ] && dbm=True
+[ $dbm == True ] && read -p "Setting up filesystem..."
 sfdisk /dev/sda < filesys
 timedatectl set-ntp true
 mkfs.ext4 -F /dev/sda1
 mkswap /dev/sda2
 swapon /dev/sda2
 mount /dev/sda1 /mnt
+[ $dbm == True ] && read -p "Installing pacman-contrib..."
 pacman -S --noconfirm pacman-contrib
+[ $dbm == True ] && read -p "Configuring mirrorlist..."
 curl -s "https://www.archlinux.org/mirrorlist/?country=US&protocol=https&ip_version=4" | sed -e 's/.Server/Server/' | rankmirrors -n 10 - > /etc/pacman.d/mirrorlist
+[ $dbm == True ] && read -p "Installing base system..."
 pacstrap -i /mnt base linux base-devel linux-firmware vim grub sudo dhcpcd xfce4 xorg-server xfce4-goodies qbittorrent firefox noto-fonts ntfs-3g gvfs virtualbox-guest-utils arc-gtk-theme papirus-icon-theme vlc base-devel unzip
 genfstab -U /mnt >> /mnt/etc/fstab
+[ $dbm == True ] && read -p "Writing second part of script to disk..."
 echo '
 ln -sf /usr/share/zoneinfo/US/Pacific /etc/localtime
 hwclock --systohc
@@ -34,6 +40,7 @@ systemctl enable dhcpcd.service
 systemctl enable vboxservice.service
 visudo
 ' > /mnt/test.sh
+[ $dbm == True ] && read -p "starting part 2..."
 chmod +x /mnt/test.sh
 arch-chroot /mnt ./test.sh
 # shutdown now
